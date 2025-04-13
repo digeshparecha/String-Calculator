d797c2255c8a0132ef0606ac23b2c75642f9e868
Digesh <digeshparecha2819@gmail.com>
2025-04-13 15:41:56 +0530

    Test: Added failing test for when input has \n with comma

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -16,5 +16,13 @@ describe StringCalculator do
         expect(subject.add('1,2,3,4')).to eq(10)
       end
     end
+
+    context 'when input is has \n with comma' do
+      it 'should return sum' do
+        expect(subject.add('1')).to eq(1)
+        expect(subject.add('1\n2')).to eq(3)
+        expect(subject.add('1,2\n3,4')).to eq(10)
+      end
+    end
   end
 end
