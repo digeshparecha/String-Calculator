e6aa315c1f842d5282474899225ea58983f6753e
Digesh <digeshparecha2819@gmail.com>
2025-04-13 15:14:11 +0530

    Test: Added failing test for add method with not empty comma separated string input

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,6 +1,19 @@
-# frozen_string_literal: true
-
 require_relative '../string_calculator'
 
 describe StringCalculator do
+  describe '#add' do
+    context 'when input is empty or nil' do
+      it 'should return 0' do
+        expect(subject.add('')).to eq(0)
+        expect(subject.add(nil)).to eq(0)
+      end
+    end
+
+    context 'when input is non-empty comma separated string' do
+      it 'should return sum' do
+        expect(subject.add('1')).to eq(1)
+        expect(subject.add('1,2')).to eq(3)
+      end
+    end
+  end
 end
