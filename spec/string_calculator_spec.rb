eca97954fceab14174eb07587ee7c1b8150cdf28
Digesh <digeshparecha2819@gmail.com>
2025-04-13 16:56:53 +0530

    Feat: Added support to custome delimiter for input string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -25,11 +25,27 @@ describe StringCalculator do
       end
     end
     
-    context 'when input is has delimiter' do
+    context 'when input has delimiter' do
       it 'should return sum' do
         expect(subject.add('1')).to eq(1)
         expect(subject.add("//?\n1?2")).to eq(3)
         expect(subject.add("//;\n1;2;3;4")).to eq(10)
+      end
+    end
+
+    contex 'when input has' do
+      contex 'single negative number' do
+        it 'should retun error message' do
+          expect(subject.add("1\n-2")).to eq(3)
+          expect(subject.add("//;\n1;2-3;4")).to eq(10)
+        end
+      end
+
+      contex 'more than more negative number' do
+        it 'should retun error message with negative numbers' do
+          expect(subject.add("1\n-2\n-3\n-4")).to eq(3)
+          expect(subject.add("//;\n1;2-3;4;-5;6;-7")).to eq(10)
+        end
       end
     end
   end
