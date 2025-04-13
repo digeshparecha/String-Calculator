1e62fdf8731daa7d3787ab9d23f3f5341b3e7963
Digesh <digeshparecha2819@gmail.com>
2025-04-13 15:20:43 +0530

    Feat: Add method will return sum of comma seperated input

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,2 +1,18 @@
 class StringCalculator
+  def add(input)
+
+    input&.strip! # remove extra space.
+
+    #return 0 if input is empty or nil
+    return 0 if input.nil? || input.empty?
+
+    sum = 0
+    
+    # sum every comma seperated element from input
+    input.split(',').each do |el|
+      sum += el.to_i
+    end
+
+    sum
+  end
 end
