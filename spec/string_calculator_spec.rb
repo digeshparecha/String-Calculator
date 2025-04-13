b636cdd10b002c5a4440cbebd966b386f5548c9c
Digesh <digeshparecha2819@gmail.com>
2025-04-13 15:26:22 +0530

    Test: Added case for add method to handle any number of length input

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -13,6 +13,7 @@ describe StringCalculator do
       it 'should return sum' do
         expect(subject.add('1')).to eq(1)
         expect(subject.add('1,2')).to eq(3)
+        expect(subject.add('1,2,3,4')).to eq(10)
       end
     end
   end
